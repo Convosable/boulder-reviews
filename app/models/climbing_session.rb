7c8819eb3b45f96f839b7a9eb6fd53726ea9ef6d
Connor Vosberg <connorjv@gmail.com>
2023-08-24 20:53:02 -0700

    updates Active Record validations for Climbing_Session model

--- app/models/climbing_session.rb
+++ app/models/climbing_session.rb
@@ -2,5 +2,9 @@ class ClimbingSession < ApplicationRecord
 
     belongs_to :user
     belongs_to :boulder_problem
-    
+
+    validates :private, presence: true
+    validates :completed, presence: true
+    validates :boulder_rating, presence: true, numericality: { in: 0..4 }
+
 end
