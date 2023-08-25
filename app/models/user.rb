d051a164ebd5ef3b771d867d2d86e16a7280bfee
Connor Vosberg <connorjv@gmail.com>
2023-08-24 20:19:05 -0700

    updates Active Record validations for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,19 @@ class User < ApplicationRecord
 
     has_secure_password
 
+    validates :username, presence: true, uniqueness: true
+    validates :password_digest, presence: true, length: { minimum: 8}
+    validates :name, presence: true
+    validates :height, presence: true, numericality: true
+    validates :weight, presence: true, numericality: true
+    validates :experience, presence: true, inclusion: { in: %w(beginner intermediate advanced elite), message: "%{value} is not a valid experience level" }
+    #look at case insensitivity for experience (or maybe a dropdown in frontend)
+
+
+    # validates :password_complexity
+
+    # def password_complexity
+    #   look up password complexities to include a punctiation/number
+    # end
+
 end
